80fe0068b9e6712a786665855685f56d4504e9ae
MaryGrace Blair <mblair021@gmail.com>
2024-02-24 15:16:10 -0600

    initial commit

---
+++ about/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+#from .forms import postform
+from django.shortcuts import redirect
+
